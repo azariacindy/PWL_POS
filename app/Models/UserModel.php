4e747276305ba6beb36eb46e582aaa3aa8d292c8
Azaria Cindy Sahasika <cindysss099@gmail.com>
2025-03-06 17:30:08 +0700

    add fillable in usermodel

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,6 @@ class UserModel extends Model
 
     protected $table = 'm_user'; // mendefiniskan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // mendefinisikan primary key dari table yang digunakan 
+    
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
