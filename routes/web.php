10da8d646812fd9efc3f029e0cea8579092f92e7
Azaria Cindy Sahasika <cindysss099@gmail.com>
2025-03-20 12:31:29 +0700

    Practicum 2- Edit Data Ajax Modal (User Data)

--- routes/web.php
+++ routes/web.php
@@ -41,10 +41,16 @@ Route::group(['prefix' => 'user'], function () {
     Route::post('/list', [UserController::class, 'list']);              // Menampilkan data user dalam bentuk json untuk datatable
     Route::get('/create', [UserController::class, 'create']);           // Menampilkan halaman form tambah user
     Route::post('/', [UserController::class, 'store']);                 // Menyimpan data user baru
+
     Route::get('/create_ajax', [UserController::class, 'create_ajax']); // Menampilkan halaman form tambah user Ajax
     Route::post('/ajax', [UserController::class, 'store_ajax']);        // Menyimpan data user baru Ajax
+
     Route::get('/{id}', [UserController::class, 'show']);               // Menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']);          // Menampilkan halaman form edit user
+
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);     // Menampilkan halaman form edit user Ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // Menyimpan perubahan data user Ajax
+    
     Route::put('/{id}', [UserController::class, 'update']);             // Menyimpan perubahan data user
     Route::delete('/{id}', [UserController::class, 'destroy']);         // Menghapus data user
 });
