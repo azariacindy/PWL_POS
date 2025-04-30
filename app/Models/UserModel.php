7d5892442e5344ac128094e7190a15221d5d0c98
Azaria Cindy Sahasika <cindysss099@gmail.com>
2025-04-30 08:01:40 +0700

    add authorization

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,19 +10,30 @@ class UserModel extends Authenticatable
 {
     use HasFactory;
 
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
+    protected $table = 'm_user'; // table yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; // primary key dari table
     protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
 
     protected $hidden = ['password']; // jangan di tampilkan saat select
 
     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
-    /**
-     * Relasi ke tabel level
-     */
+    // Relasi ke tabel level
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
+    // Mendapatkan nama role
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    // Cek apakah user memiliki role tertentu
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
+    }
+
 }
