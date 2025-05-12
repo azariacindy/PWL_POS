398322091ceaec0c402980432af68dfda8696349
Azaria Cindy Sahasika <cindysss099@gmail.com>
2025-05-12 22:12:47 +0700

    api for register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,10 +5,20 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable  implements JWTSubject
 {
     use HasFactory;
+
+        public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
 
     protected $table = 'm_user'; // table yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // primary key dari table
